e140a77d7a7b8470bec020770f8878ba6f797e79
Sudhipth V <123481484+Sudhipth@users.noreply.github.com>
2024-02-02 20:20:54 +0530

    Add files via upload

---
+++ Feb_2_Pattern2.java
@@ -0,0 +1,24 @@
+package Daily_Problem;
+
+import java.util.Scanner;
+
+public class Feb_2_Pattern2 {
+  public static void main(String[] args)
+  {
+	  System.out.println("enter how many row needed : ");
+	  Scanner sc=new Scanner(System.in);
+	  int row=sc.nextInt();
+	  pattern1(row);
+  }
+  	static void pattern1(int r)
+  	{
+  		for(int i=1;i<=r;i++)
+  		{
+  			for(int j=1;j<=r;j++)
+  			{
+  				System.out.print("*");
+  			}
+  			System.out.println();
+  		}
+  	}
+}
