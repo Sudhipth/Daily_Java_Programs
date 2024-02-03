e6508af9d7b263e79eb5efa95f8e4a315977289c
Sudhipth V <123481484+Sudhipth@users.noreply.github.com>
2024-02-03 21:10:15 +0530

    Add files via upload

---
+++ Feb_3_Leetcode.java
@@ -0,0 +1,37 @@
+package Daily_Problem;
+
+public class Feb_3_Leetcode {
+	public static void main(String[] args) {
+		int[] arr= {12,33,44,355,535,555,664,757,888};
+		int target=663;
+		System.out.println(ceiling(arr,target));
+
+	}
+	static int ceiling(int arr[], int target) {
+	    int start = 0;
+	    int end = arr.length - 1;
+
+	    while (start <= end) {
+	        int mid = start + (end - start) / 2;
+
+	        if (arr[mid] == target)
+	            return mid;
+
+	        boolean isAs = arr[start] < arr[mid];
+
+	        if (isAs) {
+	            if (arr[mid] < target)
+	                start = mid + 1;
+	            else
+	                end = mid - 1;
+	        } else {
+	            if (arr[mid] > target)
+	                start = mid + 1;
+	            else
+	                end = mid - 1;
+	        }
+	    }
+
+	    return start;
+	}
+}
